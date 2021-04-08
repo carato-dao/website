5d066c437120a786733b58e6f629470ba1d4b847
turinglabsorg <s.cataudo@turinglabs.org>
2021-04-08 11:08:07 +0200

    removing  margin

--- bmt/index.php
+++ bmt/index.php
@@ -10,7 +10,7 @@
 		<div class="bmt-page-header">
 			<?php echo $bmt_locales['dashboard']['menu']; ?>
 		</div>
-		<div class="bmt-page" style="padding:30px 0; margin-left:250px;">
+		<div class="bmt-page" style="padding:30px 0;">
 			<iframe width="100%" id="analytics" height="800" src="https://datastudio.google.com/embed/reporting/1d5aac6e-37fd-492b-96e5-b09f6cd17231/page/1M" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
 	</div>
