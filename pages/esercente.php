83bc020937aa3fb765134c0dc33b83ec3059aff0
Steve Rogers <steve.rog02@gmail.com>
2021-04-12 13:07:52 +0200

    esercente

--- pages/esercente.php
+++ pages/esercente.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="it">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BasementCMS</title>
-  </head>
-  <body>
-    <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-
-          <div class="inner cover">
-            <p class="lead">This is esercente <?php var_dump($_GET); ?></p>
-            <?php $esercente = returnDBObject("SELECT * FROM datatype_esercenti WHERE slug=?", [$_GET['slug']]); ?>
-            <?php var_dump($esercente); ?>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-  </body>
-</html>
+<?php include('components/header.php'); ?>
+<p class="lead">This is esercente <?php var_dump($_GET); ?></p>
+<?php $esercente = returnDBObject("SELECT * FROM datatype_esercenti WHERE slug=?", [$_GET['slug']]); ?>
+<?php var_dump($esercente); ?>
+<?php include('components/footer.php'); ?>
